735cf1e0ee8ed65a1049d56fe9c1350d179eb1df
Kristie Kiselyte <kristiekiselyte@Lon-Quid-Ml-001.lan>
2020-05-30 22:35:52 +0100

    added new pop up page about section

--- js/main.js
+++ js/main.js
@@ -1,75 +1,64 @@
 jQuery(document).ready(function($){
-	var secondaryNav = $('.cd-secondary-nav'),
-		secondaryNavTopPosition = secondaryNav.offset().top,
-		taglineOffesetTop = $('#cd-intro-tagline').offset().top + $('#cd-intro-tagline').height() + parseInt($('#cd-intro-tagline').css('paddingTop').replace('px', '')),
-		contentSections = $('.cd-section');
-	
-	$(window).on('scroll', function(){
-		//on desktop - assign a position fixed to logo and action button and move them outside the viewport
-		( $(window).scrollTop() > taglineOffesetTop ) ? $('#cd-logo, .cd-btn').addClass('is-hidden') : $('#cd-logo, .cd-btn').removeClass('is-hidden');
-		
-		//on desktop - fix secondary navigation on scrolling
-		if($(window).scrollTop() > secondaryNavTopPosition ) {
-			//fix secondary navigation
-			secondaryNav.addClass('is-fixed');
-			//push the .cd-main-content giving it a top-margin
-			$('.cd-main-content').addClass('has-top-margin');	
-			//on Firefox CSS transition/animation fails when parent element changes position attribute
-			//so we to change secondary navigation childrens attributes after having changed its position value
-			setTimeout(function() {
-	            secondaryNav.addClass('animate-children');
-	            $('#cd-logo').addClass('slide-in');
-				$('.cd-btn').addClass('slide-in');
-	        }, 50);
+	//cache some jQuery objects
+	var modalTrigger = $('.cd-modal-trigger'),
+		transitionLayer = $('.cd-transition-layer'),
+		transitionBackground = transitionLayer.children(),
+		modalWindow = $('.cd-modal');
+
+	var frameProportion = 1.78, //png frame aspect ratio
+		frames = transitionLayer.data('frame'), //number of png frames
+		resize = false;
+
+	//set transitionBackground dimentions
+	setLayerDimensions();
+	$(window).on('resize', function(){
+		if( !resize ) {
+			resize = true;
+			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
+		}
+	});
+
+	//open modal window
+	modalTrigger.on('click', function(event){	
+		event.preventDefault();
+		var modalId = $(event.target).attr('href');
+		transitionLayer.addClass('visible opening');
+		var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 800;
+		setTimeout(function(){
+			modalWindow.filter(modalId).addClass('visible');
+			transitionLayer.removeClass('opening');
+		}, delay);
+	});
+
+	//close modal window
+	modalWindow.on('click', '.modal-close', function(event){
+		event.preventDefault();
+		transitionLayer.addClass('closing');
+		modalWindow.removeClass('visible');
+		transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
+			transitionLayer.removeClass('closing opening visible');
+			transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+		});
+	});
+
+	function setLayerDimensions() {
+		var windowWidth = $(window).width(),
+			windowHeight = $(window).height(),
+			layerHeight, layerWidth;
+
+		if( windowWidth/windowHeight > frameProportion ) {
+			layerWidth = windowWidth;
+			layerHeight = layerWidth/frameProportion;
 		} else {
-			secondaryNav.removeClass('is-fixed');
-			$('.cd-main-content').removeClass('has-top-margin');
-			setTimeout(function() {
-	            secondaryNav.removeClass('animate-children');
-	            $('#cd-logo').removeClass('slide-in');
-				$('.cd-btn').removeClass('slide-in');
-	        }, 50);
+			layerHeight = windowHeight*1.2;
+			layerWidth = layerHeight*frameProportion;
 		}
 
-		//on desktop - update the active link in the secondary fixed navigation
-		updateSecondaryNavigation();
-	});
-
-	function updateSecondaryNavigation() {
-		contentSections.each(function(){
-			var actual = $(this),
-				actualHeight = actual.height() + parseInt(actual.css('paddingTop').replace('px', '')) + parseInt(actual.css('paddingBottom').replace('px', '')),
-				actualAnchor = secondaryNav.find('a[href="#'+actual.attr('id')+'"]');
-			if ( ( actual.offset().top - secondaryNav.height() <= $(window).scrollTop() ) && ( actual.offset().top +  actualHeight - secondaryNav.height() > $(window).scrollTop() ) ) {
-				actualAnchor.addClass('active');
-			}else {
-				actualAnchor.removeClass('active');
-			}
+		transitionBackground.css({
+			'width': layerWidth*frames+'px',
+			'height': layerHeight+'px',
 		});
+
+		resize = false;
 	}
-
-	//on mobile - open/close secondary navigation clicking/tapping the .cd-secondary-nav-trigger
-	$('.cd-secondary-nav-trigger').on('click', function(event){
-		event.preventDefault();
-		$(this).toggleClass('menu-is-open');
-		secondaryNav.find('ul').toggleClass('is-visible');
-	});
-
-	//smooth scrolling when clicking on the secondary navigation items
-	secondaryNav.find('ul a').on('click', function(event){
-        event.preventDefault();
-        var target= $(this.hash);
-        $('body,html').animate({
-        	'scrollTop': target.offset().top - secondaryNav.height() + 1
-        	}, 400
-        ); 
-        //on mobile - close secondary navigation
-        $('.cd-secondary-nav-trigger').removeClass('menu-is-open');
-        secondaryNav.find('ul').removeClass('is-visible');
-    });
-
-    //on mobile - open/close primary navigation clicking/tapping the menu icon
-	$('.cd-primary-nav').on('click', function(event){
-		if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
-	});
 });
